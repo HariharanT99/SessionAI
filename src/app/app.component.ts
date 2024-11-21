bdb2764b2ec5bc2ac0968a79e65ba3647d3689cd
HariharanT <hariharant99@gmail.com>
2024-11-21 11:33:12 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'DemoApp';
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
+
+  add() {
+    this.result = this.num1 + this.num2;
+  }
+
+  subtract() {
+    this.result = this.num1 - this.num2;
+  }
 }
